694a7e226971bbf686f870739822f0c150a79ab4
omer <omertopak1@gmail.com>
2023-12-02 04:52:02 +0000

    error

---
+++ session_3/errors.js
@@ -0,0 +1,106 @@
+"use strict";
+
+
+/* ExpressJS Start */
+const express = require('express');
+const app = express() 
+
+/* ENV */
+require('dotenv').config()  
+const PORT = process.env.PORT || 8000
+/* ------------------------------------------------------- ERROR TEST */
+
+//? app.get('/user/:id', (req, res) => {
+//?     const id = req.params.id ?? 0
+//?     if (isNaN(id)) {
+//?         res.statusCode = 400
+//?         throw new Error('ID is Not A Number', { cause: 'params.id='+id })
+//?     } else {
+//?         res.send({ 
+//?             error: false, 
+//?             id: id
+//?         })
+//?     }
+//? })
+/* ------------------------------------------------------- */
+//? app.get('/user/:id', (req, res, next) => {
+
+//?     try {
+//?         const id = req.params.id ?? 0
+//?         if (isNaN(id)) {
+//?             throw new Error('ID is Not A Number', { cause: 'params.id='+id }) //! hata var ise asagida yakaladik ve json mesaj yolladik
+//?         } else {
+//?             res.send({ 
+//?                 error: false, 
+//?                 id: id
+//?             })
+//?         }
+//?     } catch (err) {
+
+//?         console.log('try-catch runned')
+//?         // next(err) // Go to errorHandler()
+
+//?         res.send({ 
+//?             error: true, 
+//?             message: err.message,
+//?             cause: err.cause
+//?         })
+//?     }
+/* ------------------------------------------------------- */
+ app.get('/user/:id', (req, res, next) => {
+
+     try {
+         const id = req.params.id ?? 0
+         if (isNaN(id)) {
+             throw new Error('ID is Not A Number', { cause: 'params.id='+id }) //! hata var ise asagida yakaladik ve json mesaj yolladik
+         } else {
+             res.send({ 
+                 error: false, 
+                 id: id
+             })
+         }
+     } catch (err) {
+
+         console.log('try-catch runned')
+         next(err) //! Go to errorHandler()
+
+        //  res.send({ 
+        //      error: true, 
+        //      message: err.message,
+        //      cause: err.cause
+        //  })
+     }
+    })
+/* ------------------------------------------------------- */
+//*ASYNC
+const asyncHandler = require('express-async-handler')
+
+app.get('/async', asyncHandler(async (req, res, next) => {
+    res.errorStatusCode = 400
+    throw new Error('Created error in async-func')
+}))
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
+    const errorHandler = (err, req, res, next) => {
+
+    const statusCode = res.errorStatusCode ?? 500
+
+    console.log('errorHandler runned')
+
+    res.status(statusCode).send({ 
+        error: true, // special data
+        message: err.message, // Error string Message
+        cause: err.cause, // Error optional cause
+        stack: err.stack // Error Details.
+    })
+}
+//? for run errorHandler call in use.
+//? It must be at last middleware.
+app.use(errorHandler)
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
+/* ------------------------------------------------------- */
+app.listen(PORT,()=> console.log("Running: http://127.0.0.1:" + PORT)) 
+/* ------------------------------------------------------- */
