f01889424525295660b1b8aa4c1e0c5fd3cc6fd3
omer <omertopak1@gmail.com>
2023-12-04 04:12:20 +0000

    session cookie

--- session_7 usermodel/src/models/userModel.js
+++ session_7 usermodel/src/models/userModel.js
@@ -1,25 +1,30 @@
+"use strict"
 const mongoose =require('mongoose')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
 const userShema = new mongoose.Schema({
 
     email:{
         type: String,
         trim:true,
         unique:true,
-        required:[true,'email field must be required.']
-
+        required:[true,'email field must be required.'],
+        validate: [
+            (email) => (email.includes('@') && email.includes('.')), // ValidationCheck
+            'Email type is incorrect.' // If false Message.
+        ]
     },
     password:{
         type:String,
         trim:true,
         required:true,
-
+        set: (password)=> passwordEncrypt(password)
     },
     firstname: String,
     lastmname: String,
-
-},{
+},
+{
     collection:'users',
-    timestamps:true
+    timestamps:true,
 })
-
 module.exports = mongoose.model('User',userShema)
