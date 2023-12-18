ebba564fd382f1ef2b361e4cbeb982c33e3d664d
omer <omertopak1@gmail.com>
2023-12-18 18:02:43 +0000

    up

--- session_03 deneme/routes/index.js
+++ session_03 deneme/routes/index.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const express = require('express')
-const router = express.Router()
-
+// const express = require('express')
+// const router = express.Router()
+const router = require('express').Router()
 
 const func = (req,res,next) =>{
     const {user} = req.query
